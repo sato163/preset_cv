2046318e77d5faa99df94f897ef686c51995cf31
sato163 <sato.121233@gmail.com>
2022-11-26 19:09:11 +0900

    temp

--- script/train.sh
+++ script/train.sh
@@ -7,7 +7,7 @@ batch_size=1000
 epoch=0
 
 separate_onehot=100
-evaluate_batch_size=100
+evaluate_batch_size=1000
 
 # loss_type="nearest_orthogonal_loss"
 # loss_type="nearest_orthogonal_or_more_loss"
